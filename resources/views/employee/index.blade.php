b03a0f6bcbb9d71fab325aea395de7d775059a46
message-cpu <melvinsantosgulpane@gmail.com>
2025-05-26 02:06:33 -0400

    elvs

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -16,17 +16,17 @@
     <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
-        <a href="{{ route('employee.create') }}" class="btn btn-info">Add New Employee</a>
-        <div class="row">
+            <a href="{{ route('employee.create') }}" class="btn btn-info">Add New Employee</a>
+            <div class="row">
 
-       
+
                 <div class="card-head">
-                     
+
                 </div>
 
                 <div class="card-body">
 
-                    
+
 
                     <table class="table table-bordered table-stiped fs-1 text-black">
 
@@ -42,40 +42,46 @@
                             </tr>
                         </thead>
 
-                        <tbody  >
-                            @foreach ($employees)
-                            <tr>
-                                
-                                <td class="">{{$items->id}}</td>
-                                <td>{{$items->fname}}</td>
-                                <td>{{$items->lname}}</td>
-                                <td>{{$items->midname}}</td>
-                                <td>{{$items->age}}</td>
-                                <td>{{$items->address}}</td>
-                                <td>{{$items->zip}}</td>
-                                {{-- <td> 
-                                    <span class="badge bg-success"><a href="{{  route('employee.edit, $items->id')}}" class="btn btn-success mx-3  "><h5>Edit</h5></a></span>
-                                </td>
-                                <td> 
-                                    
-                                <span class="badge bg-danger"><a href="{{  route('employee.delete', $items->id)}}" class="m-3 p-lg-5"><h5>Delete</h5></a></span>
-                                </td> --}}
-                                
-                            </tr>
+                        <tbody>
+                            @foreach ($employees as $items)
+                                <tr>
+
+                                    <td class="">{{$items->id}}</td>
+                                    <td>{{$items->fname}}</td>
+                                    <td>{{$items->lname}}</td>
+                                    <td>{{$items->midname}}</td>
+                                    <td>{{$items->age}}</td>
+                                    <td>{{$items->address}}</td>
+                                    <td>{{$items->zip}}</td>
+                                    <td>
+                                        <span class="badge bg-success"><a href="{{  route('employee.edit', $items->id)}}"
+                                                class="btn btn-success mx-3  ">
+                                                <h5>Edit</h5>
+                                            </a></span>
+                                    </td>
+                                    <td>
+
+                                        <span class="badge bg-danger"><a href="{{  route('employee.delete', $items->id)}}"
+                                                class="m-3 p-lg-5">
+                                                <h5>Delete</h5>
+                                            </a></span>
+                                    </td>
+
+                                </tr>
                             @endforeach
                         </tbody>
 
                     </table>
                 </div>
-                    
+
 
                 <div class="card-footer">
-                    
-                </div>
-             
 
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+                </div>
+
+
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
 @endsection
